b254c197a7baabd8c66e7b0265a0761d2a7368a9
Mehmet Tekman <m.tekman@ucl.ac.uk>
2014-01-18 01:09:12 +0000

    WeightLogger working as it should and losing weight!

--- Weight.py
+++ Weight.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import sys
+from time import localtime as time
+
 class Weight:
 	def __init__(self,morning=-1,night=-1):
 		self.morn = morning
@@ -7,23 +10,24 @@ class Weight:
 
 class WeightLog:
 
-	def __init__(self,path="~/.config/keto/weightlog.txt"):
+	def __init__(self,path="/home/user/.config/keto/weightlog.txt"):
 		self.weightlogmap={}
 		self.path = path
-		read()
+		self.date = "%04d/%02d/%02d" % time()[0:3]
+		self.read()
 		
 	def read(self):
-		f=open(self.path,'r')
-
+                f=open(self.path,'r')
+                
 		f.readline()	#Skip header
 		for weight in f:
-			date, morn, nigh = weight.split()
-			self.weightlogmap[date] = Weight(morn,nigh)
+			date, morn, nigh = weight.split('\t')
+			self.weightlogmap[date] = Weight(int(morn),int(nigh))
 		f.close()
 
 	def write(self):
 		f=open(self.path,'w')
-		print >> f, "Date\tMorn\tNight"	#print header
+		print >> f, "\tDate\tMorn\tNight"	#print header
 
 		for date in sorted(self.weightlogmap.keys()):
 			w = self.weightlogmap[date]
@@ -31,30 +35,39 @@ class WeightLog:
 		f.close()
 
 
+	def display(self):
+		w = self.weightlogmap[self.date]
+		print >> sys.stderr, "\n\n%s\t%s\t%s" % (self.date, w.morn, w.night)
+
+
 	def log(self,lbls):
-		currdate=date()
-
 		#Weight exists for that date
-		if currdate in weightlogmap:
-			w = weightlogmap[currdate]
+		if self.date in self.weightlogmap:
+			w = self.weightlogmap[self.date]
 
-			if w.morn==-1 and w.night==-1:
-				weightlogmap[currdate].morn = lbls
-				print >> sys.stderr, "Morning lb:", lbls
+			if w.night==-1 and w.morn==-1:
+        				self.weightlogmap[self.date].morn = lbls
+	        			print >> sys.stderr, "Morning lb:", lbls
 
-			if w.morn!=-1 and w.night==-1:
-				weightlogmap[currdate].night = lbls
-				print >> sys.stderr, "Night lb:", lbls
-
+			if w.night==-1 and w.morn!=-1:
+					self.weightlogmap[self.date].night = lbls
+	        			print >> sys.stderr, "Night lb:", lbls
 			else:
 				print >> sys.stderr, "\
-Already logged weight!:",\
- weightlogmap[currdate].morn,\
- weightlogmap[currdate].night
+Already logged weight for today!:",\
+ self.weightlogmap[self.date].morn,\
+ self.weightlogmap[self.date].night
 				ans = input("Remove?\n")
 				if ans[0].lower() == 'Y':
-					weightlogmap.remove(currdate)
+					self.weightlogmap.remove(self.date)
 
 		#Weight does not exist for that date
 		else:
-			weightlogmap[currdate] = Weight(lbls)
+			self.weightlogmap[self.date] = Weight(lbls)
+       			print >> sys.stderr, "Morning lb:", lbls
+
+		self.write()
+		self.display()
+
+wl = WeightLog()
+wl.log(50)
