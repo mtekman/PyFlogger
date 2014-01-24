65bbf262cd0d961dd114da98dc0e574a1f4bc3c6
Mehmet Tekman <m.tekman@ucl.ac.uk>
2014-01-24 12:12:31 +0000

    cleanup

--- Weight.py
+++ Weight.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-from time import localtime as time
+from time import localtime, time
 from os.path import abspath
 
 class Weight:
@@ -10,16 +10,27 @@ class Weight:
 		self.night = night
 
 class WeightLog:
-
 	def __init__(self,file=abspath("../")+"/logs/keto_weightlog.txt"):
 		self.weightlogmap={}
 		self.path = file
-		self.date = "%04d/%02d/%02d" % time()[0:3]
+		
+		self.date = localtime()
+		self.today = "%04d/%02d/%02d" % self.date[0:3]
+		self.yesterday= "%04d/%02d/%02d" % localtime(time()-(24*60*60))[0:3]
+		self.nighttime= (self.date[4] > 20)
+
 		self.read()
 		
 	def read(self):
-                f=open(self.path,'r')
-                
+		try:
+			f=open(self.path,'r')
+		except IOError:
+			f=open(self.path,'w')
+			f.write("")
+			f.close()
+			return
+		
+		
 		f.readline()	#Skip header
 		for weight in f:
 			if len(weight)< 5:
@@ -27,6 +38,7 @@ class WeightLog:
 			date, morn, nigh = weight.split('\t')
 			self.weightlogmap[date] = Weight(int(morn),int(nigh))
 		f.close()
+
 
 	def write(self):
 		f=open(self.path,'w')
@@ -37,46 +49,123 @@ class WeightLog:
 			print >> f, "%s\t%s\t%s" % (date, w.morn, w.night)
 		f.close()
 
+	def display(self, date):
+		# Dates in order
+		availdates= sorted(self.weightlogmap.keys())
+		
+		#Start point
+		index = availdates.index(date)
 
-	def display(self):
-		w = self.weightlogmap[self.date]
+		print >> sys.stderr, '\n'
 		print >> sys.stderr, '='*39
 		print >> sys.stderr, "Date     \tMorn\tNight"	#print header
-		print >> sys.stderr, "%s\t%d\t%d" % (self.date, w.morn, w.night)
+
+		# Print all dates from that day forward
+		# For today it is a single date
+		for dated in availdates[index:]:
+			w = self.weightlogmap[dated]
+			print >> sys.stderr, "%s\t%d\t%d%s" % (dated, w.morn, w.night,\
+				"   <--" if date==dated else "")
 
 
-	def log(self,lbls):
+
+	def nextDay(self, date):
+		return ("%04d/%02d/%02d" % localtime(time()+(24*60*60))[0:3])
+
+
+	def checkYesterNight(self):
+		# only checking for INCOMPLETE yesterdays (i.e no yesterday, no problem)
+		if self.yesterday in self.weightlogmap:
+			if self.weightlogmap[self.yesterday].night==-1:
+				print >> sys.stderr, "Nothing logged for last night,",
+				ans = raw_input(" log that? ")
+				if ans[0].lower()=='y':
+					self.log(self.yesterday,night=True)
+				else:
+					print >> sys.stderr, "Ignoring"
+					self.weightlogmap[self.yesterday]=0
+
+	def checkTodayGaps(self):
+		if self.today in self.weightlogmap:
+			w = self.weightlogmap[self.today]
+
+			if w.morn!=-1 and w.night!=-1:
+				print >> sys.stderr, "Date logged already:"
+				self.display(self.today)
+				if raw_input("Delete? ")[0].lower() == 'y':
+					self.weightlogmap.pop(self.today)
+					print >> sys.stderr, "\rDeleted record"
+					return 0
+				return -1
+
+
+			if w.morn==-1 and w.nighttime:
+				print >> sys.stderr, "Nothing logged for this morning.",
+				if raw_input("Prepend? ")[0].lower() == 'y':
+					self.log(self.today,morning=True)
+					return 0
+
+
+	def logprompt(self):
+		self.checkYesterNight()
+		print >> sys.stderr, "\nToday's log:"
+	
+		res = self.checkTodayGaps()
+		if res!=-1:
+			self.log(self.today, not(self.nighttime))
+
+
+				
+	def log(self, date, morning):	
+		d_str=""
+		if date==self.today:
+			d_str="this morning" if morning else "tonight"
+		elif date==self.yesterday:
+			d_str="yesterday morning" if morning else "last night"
+		else:
+			d_str=date
+	
+		lbls=int(raw_input('Please give input for %s : ' % d_str))
+
 		#Weight exists for that date
-		if self.date in self.weightlogmap:
-			w = self.weightlogmap[self.date]
+		if date in self.weightlogmap:
+			w = self.weightlogmap[date]
 
-			if w.night==-1 and w.morn==-1:
-        				self.weightlogmap[self.date].morn = lbls
-	        			print >> sys.stderr, "[Logged Morning lb]"
+			if self.nighttime==False:
+				if w.morn==-1:
+					self.weightlogmap[date].morn = lbls
+					print >> sys.stderr, "[Logged Morning lb]"
+					return 0
+				print >> sys.stderr, "Wait. Already logged for morning:"
+				self.display(date)
+				if raw_input(" Update with new? ")[0].lower() == 'y':
+					self.weightlog
+				
+				
 
-			if w.night==-1 and w.morn!=-1:
-					self.weightlogmap[self.date].night = lbls
-	        			print >> sys.stderr, "[Logged Night lb]"
-			else:
-			        self.display()
-				print >> sys.stderr, "Already logged weight for today!",
+			if w.night==-1 and self.nighttime:
+				self.weightlogmap[date].night = lbls
+				print >> sys.stderr, "[Logged Night lb]"
+				return 0
+				
+			if w.morn!=-1 and w.night!=-1:
+				print >> sys.stderr, "Already logged weight for %s!" % date ,
 				ans = raw_input(" Remove? ")
 				if ans[0].lower() == 'y':
-					self.weightlogmap.pop(self.date)
+					self.weightlogmap.pop(date)
 					print >> sys.stderr, "\rDeleted record"
-					self.log(lbls)
+					self.log(date,lbls, morning=True)
 					return
-				else:
-					print >> sys.stderr, "\rUnchanged"
 
-
+				print >> sys.stderr, "\rUnchanged"
+				return
+				
 		#Weight does not exist for that date
-		else:
-			self.weightlogmap[self.date] = Weight(lbls)
-      			print >> sys.stderr, "[Logged Morning lb]"
+		self.weightlogmap[date] = Weight(lbls)
+		print >> sys.stderr, "[Logged Morning lb]"
 
 		self.write()
-		self.display()
+		self.display(date)
 
 wl = WeightLog()
-wl.log(50)
+wl.logprompt()
--- ketologger.py
+++ ketologger.py
@@ -1,19 +1,49 @@
 #!/usr/bin/env python
 
 import sys
-from time import localtime as time
-
+from FoodLogger import FoodLogger
+from Weight import WeightLog
 
 class Args:
 
+	def usage(self):
+		print >> sys.stderr, '''
+Records progress during keto; weight and food consumption
+		
+		%s <command> <task> [OPT]
+
+commands:  	insert, remove, list, plot
+tasks:    	weight, food
+
+OPTS:		foodname, lbs
+
+''' % sys.argv.split('/')[-1]
+	exit(-1)
+
 	def __init__(self,argv):
-		self.argv = argv
 		self.insert = False
-		self.update = False
 		self.remove = False
+		self.list = False
+		self.task = ""
+
+		arg=sys.argv[0]	
+		if arg.startswith('insert'):
+			self.insert=True
+		elif arg.startswith('remove') or arg.startswith('delete'):
+			self.remove=True
+		elif arg.startswith('list'):
+			self.list=True
+		elif arg.startswith('plot'):
+			self.plot=True
+	
+		arg=sys.argv[1].lower()	
+		if arg.startswith('food'):
+			self.insert=True
+		elif arg.startswith('weight'):
+			self.insert=True
+		
 
 
 	def parse(self):
 		for arg in self.argv:
 			if arg=="insert":
-				doNothing()
