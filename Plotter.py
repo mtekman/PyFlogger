437a8106269b84872dabb178141b679e53da5e33
Mehmet Tekman <m.tekman@ucl.ac.uk>
2014-01-31 13:48:06 +0000

    axes slightly fixes

--- Plotter.py
+++ Plotter.py
@@ -21,9 +21,13 @@ class Grid:
     for y in xrange(len(self.grid)):
       print reduce(lambda x,y: x+y, self.grid[y])
 
-  def writeText(self, text, y, x): 
+  def writeText(self, text, y, x):
+    try:
+      text += 1
+      text= "%d"
+    except ValueError:
     c=x
-    for let in text:
+    for let in str(text):
       self.grid[y][c] = let
       c += 1
 
@@ -34,26 +38,32 @@ class WeightPlot:
     self.rows, self.cols = map(lambda x: int(x), popen('stty size').read().split())
     self.screen = Grid(self.rows, self.cols)
 
-    self.marg=3
+    self.marg=4
 
     self.determineRanges(array_of_tuples)
     self.determineGrid()
-    print >> sys.stderr, (self.min_x, self.max_x, self.min_y, self.max_y),\
-    self.xspace_per_int, self.yspace_per_int
     
     self.setAxes()
     self.screen.printGrid()
+    print >> sys.stderr, (self.min_x, self.max_x, self.min_y, self.max_y),\
+    self.xspace_per_int, self.yspace_per_int
+	
     
 
-  def setAxes(self):
+  def setAxes(self, xresol=5, yresol=2):
+#    xresol=self.xspace_per_int
+
     for y in xrange(self.marg,self.rows-self.marg):
       for x in xrange(self.marg, self.cols-self.marg):
-        if y==(self.rows-(self.marg+1)):
+        if y==(self.rows-(self.marg)-1):
           self.screen.grid[y][x] = '_'
+          if x%xresol==0:
+             self.screen.writeText('/', y+1, x)
+             self.screen.writeText( ((self.min_x + ((x-self.marg)/self.xspace_per_int))  ), y+2, x)
         if x==self.marg:
           self.screen.grid[y][x] = '|'
-          if y%3==0:
-             self.screen.writeText(str(self.max_y - ((y+1)*self.yspace_per_int)), y, x-2)
+          if y%yresol==0:
+             self.screen.writeText( ((self.max_y - ((y-self.marg)/self.yspace_per_int)) ), y, x-2)
 
 
   def determineRanges(self, tuples):
@@ -66,13 +76,16 @@ class WeightPlot:
     self.max_y = ysorted[-1][1]
 
   def determineGrid(self):
-     self.xspace_per_int = ((self.cols-(2*self.marg))/(self.max_x - self.min_x))
-     self.yspace_per_int = ((self.rows-(2*self.marg))/(self.max_y - self.min_y))
+     self.xspace_per_int = (float(self.cols-(2*self.marg))/(self.max_x - self.min_x))
+     self.yspace_per_int = (float(self.rows-(2*self.marg))/(self.max_y - self.min_y))
+     if self.yspace_per_int==0:
+     	self.yspace_per_int = 1
 
                                                          
 w = WeightPlot([[2,5],\
 [3,9],\
 [4,9],\
 [8,1],\
-[11,-3]])
+[11,-3],\
+[11,-9]])
 
