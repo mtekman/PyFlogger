4de64692cec871c1e44bce5fc87ee96b5b61aa3c
Mehmet Tekman <m.tekman@ucl.ac.uk>
2014-06-04 15:14:11 +0100

    shall we

--- Suggest.py
+++ Suggest.py
@@ -16,8 +16,8 @@ class Suggest:
 
 
 	def suggestSomething(self):
-		suggestSingles()
-		suggestPortions()
+#		self.suggestSingles()
+		self.suggestPortions()
 
 
 	def suggestSingles(self):
@@ -34,9 +34,33 @@ class Suggest:
 
 
 	def suggestPortions(self):
-		portions = dict((x,v) for x,v in self.flist.iteritems()\
- if len(v.unit)<=2)\
- 
+		
+		def perSort(x):
+			return float(x[1].per)
+		
+		portions = dict((x,v) for x,v in self.flist.iteritems() if len(v.unit)<=2)	# g, ml, etc
+		self.portions = {}
+		
+		#scale portions to a minimum, and then chuck out the unrealistic ones
+		for name in portions:
+			yem = portions[name]
+			
+			amount_to_scale = float(self.allowed_kc)/yem.kC
+			#nearest round fraction
+			temp_scale = int(float(1)/amount_to_scale)
+			amount_to_scale = float(1)/temp_scale
+			
+			new_scale = yem.scaled(amount_to_scale)
+			
+			if new_scale.per < 20.0:
+				continue
+			
+			self.portions[name] = new_scale
+
+		sorted_ports = sorted(self.portions.iteritems(), key=perSort, reverse=True)
+
+		for x,v in sorted_ports:
+			print v.printout()
 
 
 	def checkAvail(self, perc):
